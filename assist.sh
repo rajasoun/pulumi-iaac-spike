5be280fc5198b59a6240383ac01620f03c363824
Raja Soundaramourty <rajasoun@cisco.com>
2022-07-03 14:10:53 +0000

    refactor(assist script) : assist script following stack nomenclature

--- assist.sh
+++ assist.sh
@@ -8,6 +8,9 @@ GREEN=$'\e[32m'
 BLUE=$'\e[34m'
 ORANGE=$'\x1B[33m'
 
+organization="learning-cloud"
+project="ephemeral-iaac"
+
 function check_stack_exist(){
     stack=$1
     stack_count=$(pulumi stack  ls | grep -v NAME | grep $stack | wc -l)
@@ -18,34 +21,37 @@ function check_stack_exist(){
 }
 
 function setup(){
-    stack=$1
-    pulumi stack init learning-cloud/ephemeral-iaac/$stack
+    org_proj_stack=$1
+    check_stack_exist $org_proj_stack || exit 1
+    pulumi stack init $org_proj_stack
     pulumi config set aws:region us-east-1 # any valid AWS region will work
-    pulumi stack select learning-cloud/ephemeral-iaac/$stack
-    echo -e "${GREEN}Stack $stack created successfully${NC}\n" 
+    pulumi stack select $org_proj_stack
+    echo -e "${GREEN}Stack $org_proj_stack created successfully${NC}\n" 
 }
 
 function teardown(){
-    stack=$1
-    check_stack_exist $stack || exit 1
+    org_proj_stack=$1
+    check_stack_exist $org_proj_stack || exit 1
     pulumi destroy --yes
-    pulumi stack select learning-cloud/ephemeral-iaac/$stack
-    pulumi stack rm learning-cloud/ephemeral-iaac/$stack --yes
-    echo -e "${GREEN}Stack $stack destroyed successfully${NC}\n" 
+    pulumi stack select $org_proj_stack
+    pulumi stack rm $org_proj_stack --yes
+    echo -e "${GREEN}Stack $org_proj_stack destroyed successfully${NC}\n" 
 }
 
 opt="$1"
 stack="${2:-dev}"
+org_proj_stack="$organization/$project/$stack"
+
 choice=$( tr '[:upper:]' '[:lower:]' <<<"$opt" )
 
-echo -e "\n${BLUE}Executing Action : $choice | Env: $stack ${NC}"
+echo -e "\n${BLUE}Executing Action : $choice | Env: $org_proj_stack ${NC}"
 
 case ${choice} in
     "setup")
-        setup $stack
+        setup $org_proj_stack
     ;;
     "teardown")
-        teardown $stack
+        teardown $org_proj_stack
     ;;
     *)
     echo "${RED}Usage: assist.sh < setup | teardown >  ${NC}"
