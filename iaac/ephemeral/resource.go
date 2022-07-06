da17bef18b002155be2669f3a739475cefcfc313
Raja Soundaramourty <rajasoun@cisco.com>
2022-07-06 12:05:46 +0000

    feat(secrets manager) : secrets mgmt

--- iaac/ephemeral/resource.go
+++ iaac/ephemeral/resource.go
@@ -16,12 +16,16 @@ func (aws *AWS) Manage() {
 		if err != nil {
 			return err
 		}
+		err = manageSecrets(ctx)
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 }
 
 func manageS3Bucket(ctx *pulumi.Context) error {
-	s3Conf := loadConfig(ctx)
+	s3Conf := loadS3Config(ctx)
 	// Create an AWS resource (S3 Bucket)
 	bucket, err := createBucket(ctx, s3Conf.BucketName)
 	if err != nil {
@@ -30,5 +34,18 @@ func manageS3Bucket(ctx *pulumi.Context) error {
 	}
 	// Export the name of the bucket
 	ctx.Export("bucketName", bucket.ID())
+	return nil
+}
+
+func manageSecrets(ctx *pulumi.Context) error {
+	secretsConfig := loadSecretsConfig(ctx)
+	// Create an AWS resource (S3 Bucket)
+	secret, err := createSecret(ctx, secretsConfig)
+	if err != nil {
+		log.Printf("Secret creation failed. error = %v", err)
+		return err
+	}
+	// Export the ID (in this case the ARN) of the secret
+	ctx.Export("secretContainer", secret.ID())
 	return nil
 }
